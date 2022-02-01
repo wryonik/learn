3abe09a73a6c5447b561a828dd5d218bd34859bf
shivamgupta2020 <shivam.gupta1968@gmail.com>
2022-02-01 13:28:32 +0530

    added new file

---
+++ learn/array_rotation2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[7] = {1, 2, 3, 4, 5, 6, 7};
+    int d;
+    int rot[7];
+    cout << "enter d value =";
+    scanf("%d", &d);
+    int temp[d];
+
+    for (int i = 0; i < d; i++)
+    {
+        temp[i] = arr[i];
+    }
+    for (int j = d; j < 7; j++)
+    {
+        arr[j - d] = arr[j];
+    }
+    for (int k = 0; k <= d; k++)
+    {
+        arr[7 - d + k] = temp[k];
+    }
+    for (int i = 0; i < 7; i++)
+    {
+
+        printf("%d,", arr[i]);
+    }
+    return 0;
+}
